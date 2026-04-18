1fd9134653134269cb493e16544765a9c09aff0b
Krzysztof Zaucha <krzysztofzaucha@users.noreply.github.com>
2026-04-18 20:41:25 +0100

    Update tools and docs (#7)

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -136,6 +136,16 @@ case "$1" in
     -v "${PWD}:${PWD}" \
     -w "${PWD}" \
     "${VENDOR}"/notatio:latest graphviz --input-path="docs/depgraph.dot" --output-format=svg
+  # table of contents
+  notatio toc --document-path=README.md --header="Table of contents" --limiter-right="## Summary" --index=1 \
+    int --start-from-level=1 --start-from-item=1
+  docker run --rm \
+    -v "${PWD}:${PWD}" \
+    -w "${PWD}" \
+    "${VENDOR}"/pandoc \
+    --wrap=auto --columns=120 \
+    --from=markdown-implicit_figures \
+    --to=gfm --output=README.md README.md
   # licenses
   docker run --rm \
     --name "${BASE_NAME}-golang-dev" \
@@ -149,16 +159,6 @@ case "$1" in
     --limiter-left="###" \
     --limiter-right="## License" \
     --index=1
-  # table of contents
-  notatio toc --document-path=README.md --header="Table of contents" --limiter-right="## Summary" --index=1 \
-    int --start-from-level=1 --start-from-item=1
-  docker run --rm \
-    -v "${PWD}:${PWD}" \
-    -w "${PWD}" \
-    "${VENDOR}"/pandoc \
-    --wrap=auto --columns=120 \
-    --from=markdown-implicit_figures \
-    --to=gfm --output=README.md README.md
   ;;
 
 *)
